07573cbc6f7ac00220dbded31cf4cc797dbb3c21
leandroW <leandro_antunes28@hotmail.com>
2025-04-20 01:35:58 -0300

    import pacotes

---
+++ 04/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const frase = "Hello"
+
+type ID int // criação tipos
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "Wesley"
+	e float64 = 1.2
+	f ID      = 1
+)
+
+func main() {
+	fmt.Printf("O tipo de frase é %T", f)
+}
