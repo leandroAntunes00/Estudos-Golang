4aead5e6e1f8f7588699cf70dcfe957a58b907f9
leandroW <leandro_antunes28@hotmail.com>
2025-05-03 22:07:02 -0300

    importando pacotes go

---
+++ 20/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/leandroAntunes00/Estudos-Golang"
+)
+
+func main() {
+	s := Soma(10, 20)
+	fmt.Printf("Resultado: %v", s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github/leandroAntunes00/Estudos-Golang
+
+go 1.24.2
